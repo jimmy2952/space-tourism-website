05f20766337ffc13a6d6a90c568b5dc404c6944b
jimmy2952 <jimmy2952@gmail.com>
2022-01-24 23:31:43 +0800

    complete navlink hover active

--- src/Navigation/Nav-rectangle.tsx
+++ src/Navigation/Nav-rectangle.tsx
@@ -1,33 +1,25 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Nav-rectangle.scss";
 
 export const NavRectangle = () => {
   return (
     <nav>
-      <Link to="/">
-        <div className="nav-item">
-          <text className="nav-text number">00</text>
-          <text className="nav-text">HOME</text>
-        </div>
-      </Link>
-      <Link to="/destination">
-        <div className="nav-item">
-          <text className="nav-text number">01</text>
-          <text className="nav-text">DESTINATION</text>
-        </div>
-      </Link>
-      <Link to="/crew">
-        <div className="nav-item">
-          <text className="nav-text number">02</text>
-          <text className="nav-text">CREW</text>
-        </div>
-      </Link>
-      <Link to="/technology">
-        <div className="nav-item">
-          <text className="nav-text number">03</text>
-          <text className="nav-text">TECHNOLOGY</text>
-        </div>
-      </Link>
+      <NavLink to="/" className="nav-item">
+        <div className="nav-text number">00</div>
+        <div className="nav-text">HOME</div>
+      </NavLink>
+      <NavLink to="/destination" className="nav-item">
+        <div className="nav-text number">01</div>
+        <div className="nav-text">DESTINATION</div>
+      </NavLink>
+      <NavLink to="/crew" className="nav-item">
+        <div className="nav-text number">02</div>
+        <div className="nav-text">CREW</div>
+      </NavLink>
+      <NavLink to="/technology" className="nav-item">
+        <div className="nav-text number">03</div>
+        <div className="nav-text">TECHNOLOGY</div>
+      </NavLink>
     </nav>
   );
 };
